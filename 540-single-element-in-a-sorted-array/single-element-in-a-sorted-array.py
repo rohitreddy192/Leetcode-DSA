ee024138f2bd354b32f9ce081af4dbdbb3b99380
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2023-12-15 13:56:00 +0530

    Time: 152 ms (76.82%) | Memory: 26.1 MB (45.21%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -9,16 +9,8 @@ class Solution:
         if nums[n-1]!=nums[n-2]: return nums[n-1]
         while low<=high:
             mid = (low+high)//2
-            if nums[mid]==nums[mid-1]:
-                if mid%2 == 1:
-                    low = mid+1
-                else:
-                    high = mid-1
-            elif nums[mid]==nums[mid+1]:
-                if mid%2==0:
-                    low = mid+1
-                else:
-                    high = mid-1
+            if nums[mid]==nums[mid^1]:
+                low = mid+1
             else:
-                return nums[mid]
-        return -1
+                high = mid-1
+        return nums[low]
