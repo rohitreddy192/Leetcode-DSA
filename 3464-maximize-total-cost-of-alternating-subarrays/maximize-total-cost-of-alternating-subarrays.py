aef77d5c9c6fa5aaf761fb0aaf9d4668598c09ca
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2024-06-28 16:34:29 +0530

    Time: 474 ms (91.44%) | Memory: 30.7 MB (91.18%) - LeetSync

--- 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.py
+++ 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.py
@@ -1,12 +1,13 @@
 class Solution:
     def maximumTotalCost(self, nums: List[int]) -> int:
-        @cache
-        def dp(x):
-            if x >= len(nums):
-                return 0
-            
-            ans = nums[x] + dp(x + 1)
-            if x < len(nums) - 1:
-                ans = max(ans, nums[x] - nums[x + 1] + dp(x + 2))
-            return ans
-        return dp(0)    
+        add_result = nums[0]
+        sub_result = nums[0]
+
+        for i in range(1, len(nums)):
+            temp_add = max(add_result, sub_result) + nums[i]
+            temp_sub = add_result - nums[i]
+
+            add_result = temp_add
+            sub_result = temp_sub
+
+        return max(add_result, sub_result)
