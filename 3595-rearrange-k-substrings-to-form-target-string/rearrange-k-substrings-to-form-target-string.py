91d246452df2638e70541e5c8833645056fbe80a
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2024-12-01 06:51:43 +0530

    Time: 118 ms (99.16%) | Memory: 27.8 MB (5.28%) - LeetSync

--- 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.py
+++ 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
-        n = len(s)
-        k = n//k
-        return Counter(s[i:i+k] for i in range(0,n,k)) == Counter(t[i:i+k] for i in range(0,n,k))
+        if k==1: return s==t
+        if k==len(s): return True
+        split_size = len(t)//k
+        sub_arrays_t = [t[x:x+split_size] for x in range(0,len(t),split_size)]
+        sub_arrays_s = [s[i:i + split_size] for i in range(0, len(s), split_size)]
+        return Counter(sub_arrays_t) == Counter(sub_arrays_s)
