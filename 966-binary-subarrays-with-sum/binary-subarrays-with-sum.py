5d9db5f660dc9b9784a91713dc964d562e5510ba
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2024-12-14 23:44:46 +0530

    Time: 23 ms (92.95%) | Memory: 21 MB (9.78%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def numSubarraysWithSum(self, A: List[int], S: int) -> int:
-        count_map = {0: 1}  # Initialize with 0 sum having 1 occurrence (base case)
+        count_map = {0: 1}  
         current_sum = 0
         result = 0
         
         for num in A:
             current_sum += num
             
-            # Check how many times (current_sum - S) has occurred in the past
             result += count_map.get(current_sum - S, 0)
             
-            # Update the map with the current_sum
             count_map[current_sum] = count_map.get(current_sum, 0) + 1
         
         return result
