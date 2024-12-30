782f14134570749a8aad6e398ee79930423cb73d
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2024-12-31 02:30:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (15.91%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,12 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        st = []
-        inp = "({["
-        oup = "]})"
+        stack = []
+        inp = "([{"
+        oup = ")]}"
         for i in s:
-            if i in inp:
-                st.append(i)
-            if i in oup:
-                if len(st)==0 or i=="]" and st[-1] != "["  or i == "}" and st[-1]!="{" or i==")" and st[-1] != "(": return False
-                st.pop()
-        return True if len(st) == 0 else False
+            if len(stack)>0 and (i in oup and stack[-1]==inp[oup.index(i)]):
+                stack.pop()
+            else:
+                stack.append(i)
+        return True if len(stack)==0 else False
