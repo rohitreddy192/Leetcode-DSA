eb821507f21d7645cd74caba43687c8a22ef8dfc
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-06 02:31:05 +0530

    Time: 8 ms (9.83%) | Memory: 45.7 MB (9.67%) - LeetSync

--- 1074-high-five/high-five.java
+++ 1074-high-five/high-five.java
@@ -6,12 +6,14 @@ class Solution {
         }
         List<int[]> result = new ArrayList<>();
         mpp.forEach((key,values)->{
-            Collections.sort(values,Collections.reverseOrder());
-            int cnt = 0;
-            for(int i=0;i<Math.min(values.size(),5);i++){
-                cnt += values.get(i);
-            }
-            result.add(new int[]{key,cnt/5});
+
+            result.add(new int[] {key, values.stream().sorted(Comparator.reverseOrder()).limit(5).reduce(0,Integer::sum)/5});
+            // Collections.sort(values,Collections.reverseOrder());
+            // int cnt = 0;
+            // for(int i=0;i<Math.min(values.size(),5);i++){
+            //     cnt += values.get(i);
+            // }
+            // result.add(new int[]{key,cnt/5});
         });
 
         result.sort((a,b)-> Integer.compare(a[0],b[0]));
