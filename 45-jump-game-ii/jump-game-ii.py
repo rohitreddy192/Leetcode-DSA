8229035c71525a648ac19135993936737ae383c0
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-09 23:59:25 +0530

    Time: 7 ms (81.66%) | Memory: 18.8 MB (18.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,19 +1,13 @@
 class Solution:
-    def jump(self, nums: List[int]) -> int:  
-        @lru_cache(None)
-        def solve(i):
-            if i>=len(nums)-1:
-                return 0
+    def jump(self, nums: List[int]) -> int:
+        l = r = 0
+        res = 0
 
-            if nums[i]==0: return float("inf")
-            
-            tmp = float("inf")
-            for j in range(1,nums[i]+1):
-                tmp = min(tmp, 1 + solve(i+j))
-            
-            return tmp
-
-        return solve(0)
-
-
-        
+        while r<len(nums)-1:
+            farthest = 0
+            for i in range(l,r+1):
+                farthest = max(farthest, i+nums[i])
+            l = r +1
+            r = farthest
+            res += 1
+        return res 
