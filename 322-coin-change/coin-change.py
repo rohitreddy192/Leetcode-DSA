b4e79f96402c2bbba091ef345d5e970d2c1cf0c4
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-10 00:20:42 +0530

    Time: 1259 ms (10.91%) | Memory: 452.6 MB (5.25%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,29 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def solve(i,target):
+            if target==0: return 0
+            if target<0: return amount+1
+            if i==0:
+                return amount+1 if target%coins[0]!=0 else target//coins[0]
+            return min(1 + solve(i,target-coins[i]),solve(i-1,target))
 
-        # @lru_cache(None)
-        # def solve(i,t):
-        #     if i<0 or t<0: return float("inf")
-        #     if i==0: return t//coins[i] if(t%coins[i]==0) else float("inf")
-        #     if t==0: return 0
-        #     return min(1 + solve(i,t-coins[i]), solve(i-1,t))
-        
-        # tmp = solve(len(coins)-1,amount)
-        # if amount == 0: return 0
-        # return tmp if tmp!=float("inf") else -1
-
-        n = len(coins)
-        dp = [[1e9 for _ in range(amount+1)] for _ in range(n)]
-        for i in range(n):
-            dp[i][0] = 0
-        
-        for i in range(1,amount+1):
-            if i%coins[0] == 0:
-                dp[0][i] = i//coins[0]
-        
-        for i in range(1,n):
-            for j in range(1,amount+1):
-                dp[i][j] = min(dp[i-1][j], (1+ dp[i][j-coins[i]] if j-coins[i]>=0 else 1e9))
-        tmp = dp[n-1][amount]
-        if tmp == 1e9: return -1
-        return tmp
+        tmp = solve(len(coins)-1,amount)
+        return tmp if tmp<=amount else -1
