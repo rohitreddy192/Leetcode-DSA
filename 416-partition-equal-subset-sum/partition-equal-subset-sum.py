8b3da0322f3d7cecdc1fd65c0860cb348e16d64e
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-10 00:14:19 +0530

    Time: 1356 ms (31.11%) | Memory: 33.9 MB (34.86%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,7 +4,7 @@ class Solution:
         if totSum%2!=0: return False
 
         n = len(nums)
-        
+
         @lru_cache(None)
         def solve(i,target):
             if target==0: return True
@@ -13,4 +13,18 @@ class Solution:
                 return nums[0]==target
             return solve(i-1,target) or solve(i-1,target-nums[i])
 
-        return solve(n-1,totSum//2)
+        # return solve(n-1,totSum//2)
+        halfSum = totSum//2
+        dp = [[False for i in range(halfSum+1)] for j in range(n)]
+
+        for i in range(n):
+            dp[i][0] = True
+        
+        if nums[0]<=halfSum:
+            dp[0][nums[0]] = True
+
+        for i in range(1,n):
+            for j in range(1,halfSum+1):
+                dp[i][j] = dp[i-1][j] or (dp[i-1][j-nums[i]] if j-nums[i]>=0 else False)
+
+        return dp[n-1][halfSum]
