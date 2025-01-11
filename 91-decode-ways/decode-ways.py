4559017928f2fe3ede5260116a8413277b801a69
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-12 02:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (41.73%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,19 +1,24 @@
 class Solution:
     def numDecodings(self,s):
-        if s.startswith("0"): return 0
         n = len(s)
-        @lru_cache(None)
-        def solve(i):
+        def solve(i,dp):
             if i==n: return 1
             if i>n or s[i]=="0": return 0
-            pick_1 = solve(i+1)
+
+            if i in dp: return dp[i]
+
+            pick_1 = solve(i+1,dp)
             pick_2 = 0
             if 10<=int(s[i:i+2])<=26 and i+1 < n:
-                pick_2 = solve(i+2)
+                pick_2 = solve(i+2,dp)
 
-            return pick_1 + pick_2
+            dp[i] = pick_1 + pick_2
+            return dp[i]
+        
+        dp = {}
+        return solve(0,dp)
 
-        return solve(0)
+
 
         # if not s or len(s) == 0:
         #     return 0
