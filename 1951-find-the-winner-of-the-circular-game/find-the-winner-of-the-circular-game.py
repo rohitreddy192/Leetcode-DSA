e0d7274d73e569c2e0951e982a4746c0e9cb1077
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-11 23:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.21%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,14 +1,7 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # Initialize deque with n friends
-        circle = deque(range(1, n + 1))
-
-        # Perform eliminations while more than 1 player remains
-        while len(circle) > 1:
-            # Process the first k-1 friends without eliminating them
-            for _ in range(k - 1):
-                circle.append(circle.popleft())
-            # Eliminate the k-th friend
-            circle.popleft()
-
-        return circle[0]
+        ans = 0
+        for i in range(2, n + 1):
+            ans = (ans + k) % i
+        # add 1 to convert back to 1 indexing
+        return ans + 1
