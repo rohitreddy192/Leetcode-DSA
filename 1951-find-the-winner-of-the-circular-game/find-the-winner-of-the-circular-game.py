639f358cec354f7cd1d9472a235b2d63e9be9fd1
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-11 23:41:16 +0530

    Time: 4 ms (44.27%) | Memory: 17.8 MB (19.25%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,20 +1,9 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # Initialize list of N friends, labeled from 1-N
-        circle = list(range(1, n + 1))
-
-        # Maintain the index of the friend to start the count on
-        start_index = 0
-
-        # Perform eliminations while there is more than 1 friend left
-        while len(circle) > 1:
-            # Calculate the index of the friend to be removed
-            removal_index = (start_index + k - 1) % len(circle)
-
-            # Remove the friend at removal_index
-            circle.pop(removal_index)
-
-            # Update the start_index for the next round
-            start_index = removal_index
-
-        return circle[0]
+        l = [i for i in range(1,n+1)]
+        i = 0
+        while len(l)>1:
+            n = len(l)
+            l.pop((i+k-1)%n)
+            i = (i+k-1)%n
+        return l[0]
