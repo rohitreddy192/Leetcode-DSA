abf83e0e60cb47107d010b0e9aba6d1189157954
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-01-13 00:38:41 +0530

    Time: 61 ms (35.72%) | Memory: 21.8 MB (25.34%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.py
@@ -0,0 +1,88 @@
+class Map:
+    def __init__(self,key,val,next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+
+class MyHashMap:
+
+    def __init__(self):
+        self.capacity = 100
+        self.buckets = [None for _ in range(100)]
+
+    def getBucketIndex(self,key):
+        return hash(key) % self.capacity
+
+    def put(self, key: int, value: int) -> None:
+        idx = self.getBucketIndex(key)
+        bucketNode = self.buckets[idx]
+        if not bucketNode:
+            self.buckets[idx] = Map(key,value)
+        while bucketNode:
+            if not bucketNode.next and bucketNode.key != key:
+                bucketNode.next = Map(key,value)
+                return
+            if bucketNode.key == key:
+                bucketNode.val = value
+                return
+            bucketNode = bucketNode.next
+
+
+    def get(self, key: int) -> int:
+        idx = self.getBucketIndex(key)
+        bucketNode = self.buckets[idx]
+        while bucketNode:
+            if bucketNode.key==key:
+                return bucketNode.val
+            bucketNode = bucketNode.next
+        return -1
+        
+
+    def remove(self, key: int) -> None:
+        idx = self.getBucketIndex(key)
+        bucketNode = self.buckets[idx]
+        if not bucketNode:
+            return  # Key not found
+
+        # Handle removal of the head node
+        if bucketNode.key == key:
+            self.buckets[idx] = bucketNode.next
+            return
+
+        # Traverse the chain to find and remove the node
+        prev = bucketNode
+        curr = bucketNode.next
+        while curr:
+            if curr.key == key:
+                prev.next = curr.next
+                return
+            prev, curr = curr, curr.next
+
+        
+        # bucketNode = self.buckets[self.getBucketIndex(key)]
+        # head = bucketNode
+        # if not bucketNode:
+        #     return
+        # nextNode = bucketNode.next
+        # if bucketNode.key == key:
+        #     head = nextNode
+        #     return
+        # while nextNode:
+        #     if nextNode.key == key:
+        #         head.next = nextNode.next
+        #         return
+        #     else:
+        #         nextNode = nextNode.next
+        #         head = head.next
+
+
+    
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
