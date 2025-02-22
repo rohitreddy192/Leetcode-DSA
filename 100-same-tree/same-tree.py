06c36f980b84be9c16b6b42cc8c7ed38d71965ec
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-02-22 13:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (21.46%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -7,6 +7,5 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if(not p and not q): return True
-        if(p and q) and (p.val!=q.val): return False
-        if(p and not q) or (not p and q): return False
-        return Solution().isSameTree(p.left,q.left) and Solution().isSameTree(p.right,q.right)
+        if(not p or not q): return False
+        return Solution().isSameTree(p.left,q.left) and Solution().isSameTree(p.right,q.right) and p.val == q.val
