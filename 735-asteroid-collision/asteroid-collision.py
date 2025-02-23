9ead4aeb553c6cae7c169c1c2e61261433fb02d7
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-02-24 00:34:06 +0530

    Time: 5 ms (71.92%) | Memory: 18.8 MB (86.21%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -2,17 +2,14 @@ from typing import List
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         st = []
-
         for asteroid in asteroids:
-            while st and st[-1] > 0 and asteroid < 0:  # Collision occurs only when top is positive & asteroid is negative
-                if st[-1] < abs(asteroid):  # Stack top is smaller, so it explodes
+            while st and st[-1]>0 and asteroid<0:
+                if st[-1]<abs(asteroid):
                     st.pop()
                     continue
-                elif st[-1] == abs(asteroid):  # Both asteroids destroy each other
+                elif st[-1]==abs(asteroid):
                     st.pop()
-                break  # If stack top is larger, asteroid explodes
-
-            else:  # If no collision occurs, push asteroid to stack
+                break
+            else:
                 st.append(asteroid)
-
         return st
