3812790192aa3cc169a3b2a8176e95dc511e57c9
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-02-26 05:09:42 +0530

    Time: 21 ms (25.79%) | Memory: 21.4 MB (6.11%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,19 +1,23 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return
+        return self.partition(lists, 0, len(lists) - 1)
 
-        heap = []
-        cur = ListNode()
-        dummy = cur
+    def partition(self, lists: List[Optional[ListNode]], s: int, e: int) -> Optional[ListNode]:
+        if s == e:
+            return lists[s]
+        if s < e:
+            mid = (s + e) // 2
+            l1 = self.partition(lists, s, mid)
+            l2 = self.partition(lists, mid + 1, e)
+            return self.merge(l1, l2)
+        return None
 
-        for node in lists:
-            while node:
-                heapq.heappush(heap, node.val)
-                node = node.next
-        
-        while heap:
-            cur.next = ListNode(heapq.heappop(heap))
-            cur = cur.next
-        
-        return dummy.next
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1: return l2
+        if not l2: return l1
+        if l1.val < l2.val:
+            l1.next = self.merge(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.merge(l1, l2.next)
+            return l2
