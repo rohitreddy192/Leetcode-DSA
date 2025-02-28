ed820e860b3ba03bd26d64574c040adbd0e0bd84
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-02-28 10:25:53 +0530

    Time: 1763 ms (11.84%) | Memory: 451.4 MB (7.61%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,14 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache(None)
+        memo = {}
         def profit(i,transactionsLeft, buy):
             if transactionsLeft == 0: return 0
             if i==len(prices): return 0
-
+            if (i,transactionsLeft,buy) in memo: return memo[(i,transactionsLeft,buy)]
             prof = 0
             if buy:
                 prof = max(profit(i+1,transactionsLeft,buy), -prices[i]+profit(i+1,transactionsLeft,not buy))
             else:
                 prof = max(profit(i+1,transactionsLeft,buy), prices[i]+profit(i+1,transactionsLeft-1, not buy))
+                
+            memo[(i,transactionsLeft,buy)] = prof
             return prof
         return profit(0,2,True)
