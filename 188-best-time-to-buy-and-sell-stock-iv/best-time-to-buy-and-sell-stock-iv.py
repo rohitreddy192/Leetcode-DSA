40bb246dddbd34403882f5ca70650ae4874077a9
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-02-28 10:26:41 +0530

    Time: 307 ms (10.21%) | Memory: 81.5 MB (19.63%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        memo = {}
+        def profit(i,transactionsLeft, buy):
+            if transactionsLeft == 0: return 0
+            if i==len(prices): return 0
+            if (i,transactionsLeft,buy) in memo: return memo[(i,transactionsLeft,buy)]
+            prof = 0
+            if buy:
+                prof = max(profit(i+1,transactionsLeft,buy), -prices[i]+profit(i+1,transactionsLeft,not buy))
+            else:
+                prof = max(profit(i+1,transactionsLeft,buy), prices[i]+profit(i+1,transactionsLeft-1, not buy))
+                
+            memo[(i,transactionsLeft,buy)] = prof
+            return prof
+        return profit(0,k,True)
