ef4a45755816408bcb762e588b46a393e3811f08
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-09 02:24:47 +0530

    Time: 369 ms (81.86%) | Memory: 81.9 MB (22.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -8,6 +8,7 @@ class Solution:
         dp = {}
         def solve(i,target):
             if target==0: return True
+            if target<0: return False
             if i==0: return arr[0]==target
             if (i,target) in dp: return dp[(i,target)]
             dp[(i,target)] = solve(i-1,target-arr[i]) or solve(i-1,target)
