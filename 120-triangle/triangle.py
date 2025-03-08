1b02848587305d3892c0e08553948ee24f7ac451
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-09 02:15:32 +0530

    Time: 19 ms (5.75%) | Memory: 21.4 MB (8.19%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,9 +1,11 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        @cache
+        dp = {}
         def solve(i,j):
             if i>len(triangle) or j<0 or j>len(triangle[i]): return float("inf")
             if i==len(triangle)-1: return triangle[i][j]
-            return min(triangle[i][j]+solve(i+1,j), triangle[i][j]+solve(i+1,j+1))
+            if (i,j) in dp: return dp[(i,j)]
+            dp[(i,j)] = min(triangle[i][j]+solve(i+1,j), triangle[i][j]+solve(i+1,j+1))
+            return dp[(i,j)]
         
         return solve(0,0)
