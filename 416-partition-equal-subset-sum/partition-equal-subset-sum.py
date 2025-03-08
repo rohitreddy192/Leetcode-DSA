eb7d95087ce716e369e588a645d69a03c4d04379
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-09 02:24:30 +0530

    Time: 2077 ms (15.88%) | Memory: 373.2 MB (8.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,30 +1,16 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        totSum = sum(nums)
+    def canPartition(self, arr: List[int]) -> bool:
+        totSum = sum(arr)
         if totSum%2!=0: return False
 
-        n = len(nums)
-
-        @lru_cache(None)
+        n = len(arr)
+        target = totSum//2
+        dp = {}
         def solve(i,target):
             if target==0: return True
-            if target<0: return False
-            if i==0:
-                return nums[0]==target
-            return solve(i-1,target) or solve(i-1,target-nums[i])
-
-        # return solve(n-1,totSum//2)
-        halfSum = totSum//2
-        dp = [[False for i in range(halfSum+1)] for j in range(n)]
-
-        for i in range(n):
-            dp[i][0] = True
+            if i==0: return arr[0]==target
+            if (i,target) in dp: return dp[(i,target)]
+            dp[(i,target)] = solve(i-1,target-arr[i]) or solve(i-1,target)
+            return dp[(i,target)]
         
-        if nums[0]<=halfSum:
-            dp[0][nums[0]] = True
-
-        for i in range(1,n):
-            for j in range(1,halfSum+1):
-                dp[i][j] = dp[i-1][j] or (dp[i-1][j-nums[i]] if j-nums[i]>=0 else False)
-
-        return dp[n-1][halfSum]
+        return solve(len(arr)-1,target)
