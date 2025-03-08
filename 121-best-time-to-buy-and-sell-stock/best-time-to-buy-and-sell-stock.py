3304ae6a2d73c00bce8ed75d730b2959e2898ea7
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-08 18:18:20 +0530

    Time: 1296 ms (5.02%) | Memory: 490.8 MB (9.07%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
-        maxProfit = 0
-        7, 1, 5, 3, 6, 4 
-        for todaysPrice in prices[1:]:
-            maxProfit = max(maxProfit, todaysPrice - buy )
-            buy = min(buy,todaysPrice)
-        return maxProfit
+        @lru_cache(None)
+        def solve(i,t, buy):
+            if i==len(prices) or not t:
+                return 0
+            
+            profit = 0
+            if not buy:
+                profit = max(-prices[i] + solve(i+1,t,True), solve(i+1,t,buy))
+            elif buy:
+                profit += max(prices[i]+solve(i+1,False, buy), solve(i+1,t,buy))
+            return profit
+        return solve(0,True, False)
