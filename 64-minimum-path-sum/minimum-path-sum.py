cb004c35037e0e699d860d85c878a826ef14133c
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-09 02:10:50 +0530

    Time: 49 ms (14.05%) | Memory: 28.5 MB (6.54%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        @cache
+        def solve(i,j):
+            if i<0 or j<0: return float("inf")
+            if i==0 and j==0: return grid[0][0]
+
+            return min(grid[i][j]+solve(i-1,j), grid[i][j]+solve(i,j-1))
+        
+        return solve(len(grid)-1,len(grid[0])-1)
