66b85369e16c1f44d4d3e13e9a6e5a9ad12c1f54
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-13 08:38:08 +0530

    Time: 27 ms (45.64%) | Memory: 18.4 MB (5.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,23 +1,65 @@
 class Solution:
-    def subArrayRanges(self, nums: List[int]) -> int:
-        n, answer = len(nums), 0 
-        stack = []
-        
-        # Find the sum of all the minimum.
-        for right in range(n + 1):
-            while stack and (right == n or nums[stack[-1]] >= nums[right]):
-                mid = stack.pop()
-                left = -1 if not stack else stack[-1]
-                answer -= nums[mid] * (mid - left) * (right - mid)
-            stack.append(right)
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        MOD = 10**9+7
+        def pse(arr):
+            psee = [i for i in range(n)]
+            st = []
+            for i in range(0,n):
+                while st and arr[st[-1]]>arr[i]:
+                    st.pop()
+                psee[i] = -1 if not st else st[-1]
+                st.append(i)
+            return psee
+        def nse(arr):
+            nsee = [i for i in range(n)]
+            st = []
+            for i in range(n-1,-1,-1):
+                while st and arr[st[-1]]>=arr[i]:
+                    st.pop()
+                nsee[i] = n if not st else st[-1]
+                st.append(i)
+            return nsee
 
-        # Find the sum of all the maximum.
-        stack.clear()
-        for right in range(n + 1):
-            while stack and (right == n or nums[stack[-1]] <= nums[right]):
-                mid = stack.pop()
-                left = -1 if not stack else stack[-1]
-                answer += nums[mid] * (mid - left) * (right - mid)
-            stack.append(right)
-        
-        return answer
+        pse = pse(arr)
+        nse = nse(arr)
+        cnt = 0
+        for i in range(n):
+            left = i - pse[i]
+            right = nse[i] - i
+            cnt = (cnt + (left*right*arr[i]))
+        return cnt
+
+    def sumSubarrayMaxs(self, arr: List[int]) -> int:
+        n = len(arr)
+        MOD = 10**9+7
+        def ple(arr):
+            plee = [i for i in range(n)]
+            st = []
+            for i in range(0,n):
+                while st and arr[st[-1]]<arr[i]:
+                    st.pop()
+                plee[i] = -1 if not st else st[-1]
+                st.append(i)
+            return plee
+        def nle(arr):
+            nlee = [i for i in range(n)]
+            st = []
+            for i in range(n-1,-1,-1):
+                while st and arr[st[-1]]<=arr[i]:
+                    st.pop()
+                nlee[i] = n if not st else st[-1]
+                st.append(i)
+            return nlee
+
+        ple = ple(arr)
+        nle = nle(arr)
+        cnt = 0
+        for i in range(n):
+            left = i - ple[i]
+            right = nle[i] - i
+            cnt = (cnt + (left*right*arr[i]))
+        return cnt
+    def subArrayRanges(self, nums: List[int]) -> int:
+        MOD = 10**9 + 7
+        return (self.sumSubarrayMaxs(nums) - self.sumSubarrayMins(nums))
