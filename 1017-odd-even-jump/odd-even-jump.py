89bc2d5cae0b1f66c1c77b10c2e7f7fde6e9dd2b
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-13 08:28:20 +0530

    Time: 152 ms (42.91%) | Memory: 23.1 MB (43.09%) - LeetSync

--- 1017-odd-even-jump/odd-even-jump.py
+++ 1017-odd-even-jump/odd-even-jump.py
@@ -1,53 +1,23 @@
 class Solution:
-    def oddEvenJumps(self, arr: List[int]) -> int:
-        def odd_even_jumps(arr):
-            n = len(arr)
-            # Precompute the next index for odd jumps:
-            next_odd = [None] * n
-            # We sort indices by (arr[i], i) so that for a given arr[i] we can jump to the smallest valid index j.
-            indices = sorted(range(n), key=lambda i: (arr[i], i))
-            stack = []
-            for i in indices:
-                while stack and i > stack[-1]:
-                    idx = stack.pop()
-                    next_odd[idx] = i
-                stack.append(i)
+    def oddEvenJumps(self, A):
+        n = len(A)
+        next_higher, next_lower = [0] * n, [0] * n
 
-            # Precompute the next index for even jumps:
-            next_even = [None] * n
-            # We sort indices by (-arr[i], i) so that for a given arr[i] we can jump to the smallest index j
-            # with the largest possible value not exceeding arr[i].
-            indices = sorted(range(n), key=lambda i: (-arr[i], i))
-            stack = []
-            for i in indices:
-                while stack and i > stack[-1]:
-                    idx = stack.pop()
-                    next_even[idx] = i
-                stack.append(i)
+        stack = []
+        for a, i in sorted([a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_higher[stack.pop()] = i
+            stack.append(i)
 
-            # Recursion with memoization using lru_cache.
-            from functools import lru_cache
+        stack = []
+        for a, i in sorted([-a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_lower[stack.pop()] = i
+            stack.append(i)
 
-            @lru_cache(maxsize=None)
-            def canReach(i, odd_jump):
-                # If we are at the last index, we have reached the end.
-                if i == n - 1:
-                    return True
-
-                if odd_jump:
-                    nxt = next_odd[i]
-                    if nxt is not None:
-                        return canReach(nxt, not odd_jump)
-                    else:
-                        return False
-                else:
-                    nxt = next_even[i]
-                    if nxt is not None:
-                        return canReach(nxt, not odd_jump)
-                    else:
-                        return False
-
-            # Count the number of starting indices (starting with an odd jump)
-            count = sum(canReach(i, True) for i in range(n))
-            return count
-        return odd_even_jumps(arr)
+        higher, lower = [0] * n, [0] * n
+        higher[-1] = lower[-1] = 1
+        for i in range(n - 1)[::-1]:
+            higher[i] = lower[next_higher[i]]
+            lower[i] = higher[next_lower[i]]
+        return sum(higher)
