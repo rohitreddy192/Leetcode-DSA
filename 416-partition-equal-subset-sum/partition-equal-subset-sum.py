a7862e51e0b1905868285a87ef642baecc75a7b2
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-15 01:10:14 +0530

    Time: 1867 ms (19.64%) | Memory: 372.2 MB (8.45%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,11 +3,14 @@ class Solution:
         tot = sum(arr)
         if tot%2==1: return False
 
-        @lru_cache(None)
+        dp = {}
         def solve(i,t):
             if t==0: return True
             if i==0: return arr[0]==t
 
-            return solve(i-1,t) or (solve(i-1,t-arr[i]) if arr[i]<=t else False)
+            if (i,t) in dp: return dp[(i,t)]
+            dp[(i,t)] = solve(i-1,t) or (solve(i-1,t-arr[i]) if arr[i]<=t else False)
+
+            return dp[(i,t)]
         
         return solve(len(arr)-1,tot//2)
