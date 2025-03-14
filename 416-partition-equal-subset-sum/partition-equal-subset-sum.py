8076c55c08c99358684a7389ac01e823daddb73b
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-15 01:09:17 +0530

    Time: 989 ms (41.41%) | Memory: 382.1 MB (8.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,17 +1,13 @@
 class Solution:
     def canPartition(self, arr: List[int]) -> bool:
-        totSum = sum(arr)
-        if totSum%2!=0: return False
+        tot = sum(arr)
+        if tot%2==1: return False
 
-        n = len(arr)
-        target = totSum//2
-        dp = {}
-        def solve(i,target):
-            if target==0: return True
-            if target<0: return False
-            if i==0: return arr[0]==target
-            if (i,target) in dp: return dp[(i,target)]
-            dp[(i,target)] = solve(i-1,target-arr[i]) or solve(i-1,target)
-            return dp[(i,target)]
+        @cache
+        def solve(i,t):
+            if t==0: return True
+            if i==0: return arr[0]==t
+
+            return solve(i-1,t) or (solve(i-1,t-arr[i]) if arr[i]<=t else False)
         
-        return solve(len(arr)-1,target)
+        return solve(len(arr)-1,tot//2)
