5f5d95a8326ddd2f5332336945ca172c3c50fae4
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-16 14:32:31 +0530

    Time: 11 ms (28.58%) | Memory: 19.6 MB (9.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,25 +1,25 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        adj = [[] for i in range(len(isConnected))]
-        
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected)):
+        n = len(isConnected)
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
                 if isConnected[i][j]==1:
                     adj[i].append(j)
                     adj[j].append(i)
         
-        vis = [False]*len(isConnected)
-
-        def dfs(node, vis):
-            vis[node] = True
-            for neighbor in adj[node]:
-                if not vis[neighbor]:
-                    dfs(neighbor,vis)
+        vis = [False]*(n)
+        def dfs(i):
+            vis[i] = True
+            for node in adj[i]:
+                if not vis[node]:
+                    dfs(node)
         
         cnt = 0
-        for i in range(len(isConnected)):
+        for i in range(n):
             if not vis[i]:
+                dfs(i)
                 cnt += 1
-                dfs(i,vis)
+        
         return cnt
-
