19a3227cab7840e0fc46173e75dc45e339383df0
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-22 19:48:23 +0530

    Time: 7 ms (68.39%) | Memory: 18.3 MB (10.16%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,19 +1,15 @@
 class Solution(object):
     def isMatch(self, text: str, pattern: str) -> bool:
-        dp = {}
         n, m = len(text), len(pattern)
+        @lru_cache(None)
         def solve(i,j):
             if i>=n and j>=m: return True
             if j>=m: return False
-            if (i,j) in dp: return dp[(i,j)]
-
             match = i<n and (text[i]==pattern[j] or pattern[j]==".")
             if j+1<m and pattern[j+1]=="*":
-                dp[(i,j)] = (match and solve(i+1,j)) or solve(i,j+2)
-                return dp[(i,j)]
+                return (match and solve(i+1,j)) or solve(i,j+2)
             if match:
-                dp[(i,j)] = solve(i+1,j+1)
-                return dp[(i,j)]
-            dp[(i,j)] = False
-            return dp[(i,j)]
+                return solve(i+1,j+1)
+            return False
+        
         return solve(0,0)
