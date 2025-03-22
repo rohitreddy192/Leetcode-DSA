b0b511578be1a49531e864ffeef259b03ded9437
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-03-22 23:10:40 +0530

    Time: 243 ms (12.59%) | Memory: 30 MB (71.79%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,16 +1,39 @@
+from typing import List
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = [-1]
-        max_area = 0
-        for i in range(len(heights)):
-            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
-                current_height = heights[stack.pop()]
-                current_width = i - stack[-1] - 1
-                max_area = max(max_area, current_height * current_width)
-            stack.append(i)
+        n = len(heights)
+        
+        # Next Smaller Element (NSE)
+        def nse():
+            stack = []
+            res = [n] * n  # Default next smaller index is 'n' (out of bounds)
+            for i in range(n - 1, -1, -1):
+                while stack and heights[stack[-1]] >= heights[i]:
+                    stack.pop()
+                if stack:
+                    res[i] = stack[-1]
+                stack.append(i)
+            return res
+        
+        # Previous Smaller Element (PSE)
+        def pse():
+            stack = []
+            res = [-1] * n  # Default previous smaller index is '-1' (out of bounds)
+            for i in range(n):
+                while stack and heights[stack[-1]] >= heights[i]:
+                    stack.pop()
+                if stack:
+                    res[i] = stack[-1]
+                stack.append(i)
+            return res
+        
+        next_smaller = nse()
+        prev_smaller = pse()
 
-        while stack[-1] != -1:
-            current_height = heights[stack.pop()]
-            current_width = len(heights) - stack[-1] - 1
-            max_area = max(max_area, current_height * current_width)
+        max_area = 0
+        for i in range(n):
+            width = next_smaller[i] - prev_smaller[i] - 1
+            max_area = max(max_area, heights[i] * width)
+        
         return max_area
