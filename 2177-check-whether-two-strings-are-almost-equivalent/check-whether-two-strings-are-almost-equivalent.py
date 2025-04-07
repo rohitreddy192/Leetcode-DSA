3c0601306005b4bfde27016ba3d292fcf385e6b6
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-04-08 01:25:41 +0530

    Time: 2 ms (35.65%) | Memory: 17.9 MB (20.69%) - LeetSync

--- 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.py
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.py
@@ -2,9 +2,7 @@ class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
         cnt1 = Counter(word1)
         cnt2 = Counter(word2)
-        print(cnt1)
-        print(cnt2)
-        print(chr(97+25))
+
         for i in range(0,26):
             tmp = chr(97+i)
             if tmp in cnt1 and tmp in cnt2:
