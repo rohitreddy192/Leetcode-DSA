4962287c4b9169df33488364ff354473932bd38d
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-06-14 09:47:30 +0530

    Time: 143 ms (11.48%) | Memory: 26.9 MB (77.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache(None)
-        def solve(i,t, buy):
-            if i==len(prices) or not t:
-                return 0
-            
-            profit = 0
-            if not buy:
-                profit = max(-prices[i] + solve(i+1,t,True), solve(i+1,t,buy))
-            elif buy:
-                profit += max(prices[i]+solve(i+1,False, buy), solve(i+1,t,buy))
-            return profit
-        return solve(0,True, False)
+        buy = prices[0]
+        sell = 0
+        for price in prices[1:]:
+            sell = max(price-buy, sell)
+            buy = min(price,buy)
+        
+        return sell
