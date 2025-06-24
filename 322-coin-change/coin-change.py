99565a94aa269af0cd2156f2d3daf6da2dadb7a0
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-06-25 00:48:18 +0530

    Time: 1351 ms (9.71%) | Memory: 490.8 MB (5.02%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,12 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def solve(i,target):
+        
+        @cache
+        def solve(idx, target):
             if target==0: return 0
-            if target<0: return amount+1
-            if i==0:
-                return amount+1 if target%coins[0]!=0 else target//coins[0]
-            return min(1 + solve(i,target-coins[i]),solve(i-1,target))
+            if target<0: return float("inf")
+            if idx==0: return target//coins[idx] if target%coins[idx]==0 else float("inf")
 
-        tmp = solve(len(coins)-1,amount)
+            return min(1 + solve(idx,target-coins[idx]), solve(idx-1,target))
+        
+        tmp = solve(len(coins)-1, amount)
         return tmp if tmp<=amount else -1
