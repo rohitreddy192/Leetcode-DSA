912cc44aa0bf4614c93a17845342c1ee86e15c4a
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-01 18:50:26 +0530

    Time: 3 ms (81.85%) | Memory: 17.8 MB (44.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,29 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
+        n, m =len(grid), len(grid[0])
         q = deque()
-        freshCnt = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j]==2:
-                    q.append((0,(i,j)))
-                if grid[i][j]==1:
-                    freshCnt += 1
+                if grid[i][j] == 2:
+                    q.append([0,i,j])
         maxi = 0
-        dx = [-1,0,1,0]
-        dy = [0,-1,0,1]
         while q:
-            time, node = q.popleft()
-            x, y = node
-            maxi = max(time,maxi)
+            time, x, y = q.popleft()
+            dx, dy = [-1,0,1,0], [0,-1,0,1]
             for i in range(4):
-                nrow, ncol = x+dx[i], y+dy[i]
-                if 0<=nrow<n and 0<=ncol<m and grid[nrow][ncol]==1:
-                    grid[nrow][ncol]=2
-                    freshCnt -= 1
-                    q.append((time+1,(nrow,ncol)))
+                nx, ny = x+dx[i], y+dy[i]
+                if 0<=nx<n and 0<=ny<m and grid[nx][ny]==1:
+                    q.append([time+1,nx,ny])
+                    grid[nx][ny]=2
+                    maxi = max(maxi, time+1)
         
-        print(grid)
-
-        return maxi if freshCnt==0 else -1
-            
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    return -1
+        return maxi
