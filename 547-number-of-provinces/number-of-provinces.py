b2592060f1ed702053ae52a3e1da02bc8f85b713
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-01 18:21:57 +0530

    Time: 8 ms (35.46%) | Memory: 19.2 MB (45.76%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,25 +1,27 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        adj = [[] for _ in range(n)]
-
+    def convertToAdjList(self, isConnected):
+        n,m = len(isConnected), len(isConnected[0])
+        adjList = [[] for i in range(n)]
         for i in range(n):
-            for j in range(n):
+            for j in range(m):
                 if isConnected[i][j]==1:
-                    adj[i].append(j)
-                    adj[j].append(i)
-        
-        vis = [False]*(n)
-        def dfs(i):
-            vis[i] = True
-            for node in adj[i]:
-                if not vis[node]:
-                    dfs(node)
-        
+                    adjList[i].append(j)
+                    adjList[j].append(i)
+        return adjList
+
+    def dfs(self, adjList, vis, i):
+        vis[i] = True
+        for j in adjList[i]:
+            if not vis[j]:
+                self.dfs(adjList,vis,j)
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        adjList = self.convertToAdjList(isConnected)
         cnt = 0
+        n = len(isConnected)
+        vis = [False]*(n)
         for i in range(n):
             if not vis[i]:
-                dfs(i)
+                self.dfs(adjList, vis, i)
                 cnt += 1
-        
         return cnt
