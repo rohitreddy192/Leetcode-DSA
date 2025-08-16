975dfc5bfeffad61171fe7d264fd5e1c010dc7ce
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-16 23:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.79%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,23 +1,27 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        def swap(nums,i,j):
-            nums[i], nums[j] = nums[j], nums[i]
-        def reverse(nums,i,j):
-            while(i<j):
-                swap(nums,i,j)
-                i += 1
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i + 1] <= nums[i]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
                 j -= 1
-        ind = -1
-        n = len(nums)
-        for i in range(n-2,-1,-1):
-            if nums[i]<nums[i+1]:
-                ind = i
-                break
-        if ind==-1:
-            reverse(nums,0,n-1)
-            return nums
-        for i in range(n-1,ind,-1):
-            if nums[i]>nums[ind]:
-                swap(nums,i,ind)
-                reverse(nums,ind+1,n-1)
-                return nums
+            self.swap(nums, i, j)
+        self.reverse(nums, i + 1)
+
+    def reverse(self, nums, start):
+        i, j = start, len(nums) - 1
+        while i < j:
+            self.swap(nums, i, j)
+            i += 1
+            j -= 1
+
+    def swap(self, nums, i, j):
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
