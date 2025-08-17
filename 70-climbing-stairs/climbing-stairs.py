326142b57570ba8f68085a400230c2cc4ce99033
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-17 17:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,38 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @lru_cache(None)
+        @cache
         def solve(n):
-            if n<=2:
-                return n
-            return solve(n-2) + solve(n-1) 
-
+            if n<=2: return n
+            return solve(n-1) + solve(n-2)
         return solve(n)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # @lru_cache(maxsize=None)
-        # def solve(ind):
-        #     if ind==0: return 1
-        #     if ind<0: return 0
-        #     two_step = 0
-        #     one_step = solve(ind-1)
-        #     if ind>1:
-        #         two_step = solve(ind-2)
-        #     return one_step + two_step
-        # return solve(n)
