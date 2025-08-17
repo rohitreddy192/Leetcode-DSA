3db7c091061b71c207b3f40269ad778086225f47
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-17 17:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.90%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [-1]*(n)
-        dp[0] = nums[0]
-        if n == 1: return nums[0]
-        dp[1] = max(nums[0],nums[1])
-        for i in range(2,n):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-        return dp[n-1]
+        @cache
+        def solve(i):
+            if i<0: return 0
+            pick = nums[i] + solve(i-2)
+            not_pick = solve(i-1)
+            return max(pick, not_pick)
+        return solve(len(nums)-1)
