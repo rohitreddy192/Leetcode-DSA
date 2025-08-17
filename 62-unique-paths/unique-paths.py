ae93f5772f535f19015664c3e1b09e745222e349
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-17 07:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.09%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,8 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def solve(i,j):
-            if i==0 and j==0: return 1
-            if i<0 or j<0: return 0
-            return solve(i-1,j) + solve(i,j-1)
-        return solve(m-1,n-1)
+
+        def fac(num):
+            factorial = 1
+            for i in range(1, num+1):
+                factorial *= i
+            return factorial 
+
+        # Number of ways you can arrange c cols and r rows = (r+c)!/r!c!
+        r = m - 1
+        d = n - 1
+        return fac(r+d)//(fac(r)*fac(d))    
