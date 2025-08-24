3c80f3093e35f18d032ce8be58361b2802dcb432
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-24 08:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.10%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -6,7 +6,7 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
-        while curr and curr.next and head:
+        while curr and curr.next:
             head = head.next
             curr = curr.next.next
         
