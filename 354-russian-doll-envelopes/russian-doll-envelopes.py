75eeb47838668b35347082451a873274d0a8273e
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2025-08-31 23:30:06 +0530

    Time: 235 ms (20.76%) | Memory: 52.3 MB (99.76%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -1,5 +1,3 @@
-from bisect import bisect_left
-
 class Solution:
     def maxEnvelopes(self, arr: List[List[int]]) -> int:
         arr.sort(key=lambda x: (x[0], -x[1]))
@@ -9,12 +7,15 @@ class Solution:
             dp = []
             for i in range(len(nums)):
                 idx = bisect_left(dp, nums[i])
-                if idx == len(dp):
+                if idx==len(dp):
                     dp.append(nums[i])
                 else:
                     dp[idx] = nums[i]
+            
             return len(dp)
+        
         return lis([i[1] for i in arr])
+
 
 
 
