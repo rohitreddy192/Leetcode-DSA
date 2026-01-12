00bd8726da03d5c16657bb79dd7d13760b1d650e
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-01-12 23:16:11 +0530

    Time: 17 ms (54.50%) | Memory: 19.9 MB (7.35%) - LeetSync

--- 1176-design-a-leaderboard/design-a-leaderboard.py
+++ 1176-design-a-leaderboard/design-a-leaderboard.py
@@ -1,61 +1,55 @@
-class Node:
-    def __init__(self, score=0):
-        self.score = score
-        self.prev = None
-        self.next = None
-
+from sortedcontainers import SortedDict
 
 class Leaderboard:
 
     def __init__(self):
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-        self.player_node_map = {}
-        self.player_score_map = {}
-
-    def _remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    def _clearPlayerIdAndScore(self, playerId):
-        if playerId in self.player_node_map:
-            self._remove(self.player_node_map[playerId])
-            del self.player_node_map[playerId]
+        self.scores = {}
+        self.sortedScores = SortedDict()
 
     def addScore(self, playerId: int, score: int) -> None:
-        old_score = self.player_score_map.get(playerId, 0)
-        new_score = old_score + score
-        self.player_score_map[playerId] = new_score
 
-        self._clearPlayerIdAndScore(playerId)
-
-        curr = self.tail.prev
-        while curr != self.head and new_score < curr.score:
-            curr = curr.prev
-
-        new_node = Node(new_score)
-        new_node.next = curr.next
-        new_node.prev = curr
-        curr.next.prev = new_node
-        curr.next = new_node
-
-        self.player_node_map[playerId] = new_node
-
+        # The scores dictionary simply contains the mapping from the
+        # playerId to their score. The sortedScores contain a BST with 
+        # key as the score and value as the number of players that have
+        # that score.     
+        if playerId not in self.scores:
+            self.scores[playerId] = score
+            self.sortedScores[-score] = self.sortedScores.get(-score, 0) + 1
+        else:
+            preScore = self.scores[playerId]
+            val = self.sortedScores.get(-preScore)
+            if val == 1:
+                del self.sortedScores[-preScore]
+            else:
+                self.sortedScores[-preScore] = val - 1    
+            
+            newScore = preScore + score
+            self.scores[playerId] = newScore
+            self.sortedScores[-newScore] = self.sortedScores.get(-newScore, 0) + 1
+        
     def top(self, K: int) -> int:
-        curr = self.tail.prev
-        total = 0
+        count, total = 0, 0
 
-        while curr != self.head and K > 0:
-            total += curr.score
-            curr = curr.prev
-            K -= 1
-
+        for key, value in self.sortedScores.items():
+            times = self.sortedScores.get(key)
+            for _ in range(times): 
+                total += -key
+                count += 1
+                
+                # Found top-K scores, break.
+                if count == K:
+                    break
+                
+            # Found top-K scores, break.
+            if count == K:
+                break
+        
         return total
 
     def reset(self, playerId: int) -> None:
-        if playerId in self.player_score_map:
-            del self.player_score_map[playerId]
-        self._clearPlayerIdAndScore(playerId)
+        preScore = self.scores[playerId]
+        if self.sortedScores[-preScore] == 1:
+            del self.sortedScores[-preScore]
+        else:
+            self.sortedScores[-preScore] -= 1
+        del self.scores[playerId]
