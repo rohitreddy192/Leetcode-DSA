3c7fd76853fa529444e17390cac24db047f7abd5
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-14 01:20:01 +0530

    Time: 4 ms (7.44%) | Memory: 20 MB (6.86%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        n, m = len(obstacleGrid), len(obstacleGrid[0])
+        if obstacleGrid[0][0]==1: return 0
+        
+        @cache
+        def solve(i,j):
+            if i==0 and j==0: return 1
+            if i<0 or j<0: return 0
+            if obstacleGrid[i][j]==1: return 0
+            return solve(i-1,j) + solve(i,j-1)
+
+        return solve(n-1,m-1)
