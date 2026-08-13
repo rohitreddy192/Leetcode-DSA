3d8ce2caab237e2cc441320f469d6269874ddc1d
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-13 23:13:36 +0530

    Time: 2 ms (2.57%) | Memory: 19.1 MB (86.64%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        @cache
+        def solve(i):
+            if i<=2:
+                return i
+            
+            return solve(i-1) + solve(i-2)
+
+        return solve(n)
