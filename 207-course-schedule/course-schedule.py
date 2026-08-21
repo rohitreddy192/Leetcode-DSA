0b26367e3a018b2bfa681fff342afb55f0f2d84b
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-21 22:41:05 +0530

    Time: 11 ms (11.86%) | Memory: 20.3 MB (78.34%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = defaultdict(int)
+        adj = [[] for _ in range(numCourses)]
+        vis = set()
+        for u,v in prerequisites:
+            adj[u].append(v)
+            indegree[v] += 1
+        
+        dq = deque()
+        ans = []
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dq.append(i)
+                ans.append(i)
+                vis.add(i)
+        
+        while dq:
+            
+            node = dq.popleft()
+            for nxt_node in adj[node]:
+                indegree[nxt_node] -= 1
+                if indegree[nxt_node]==0:
+                    dq.append(nxt_node)
+                    ans.append(nxt_node)
+        
+        return len(ans)==numCourses
+                
