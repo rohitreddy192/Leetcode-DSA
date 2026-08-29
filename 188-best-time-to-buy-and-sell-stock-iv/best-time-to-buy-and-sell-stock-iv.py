a550781bc43735e5df45f8bb5b8910404540ff74
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-29 16:07:31 +0530

    Time: 229 ms (12.30%) | Memory: 101.8 MB (5.90%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        @cache
+        def solve(idx, buy, transactionsLeft):
+            if idx==n:
+                return 0
+            
+            if transactionsLeft == 0:
+                return 0
+                
+            if not buy and transactionsLeft>0:
+                return max(-prices[idx] + solve(idx+1, True, transactionsLeft), solve(idx+1, buy, transactionsLeft))
+
+            else:
+                return max(prices[idx]+solve(idx+1,False, transactionsLeft-1), solve(idx+1, buy, transactionsLeft))
+
+
+        return solve(0,False,k)
