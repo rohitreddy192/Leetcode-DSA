8d5ec763f46d5d32fb18be22aa14d02a175254ee
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-30 09:55:00 +0530

    Time: 337 ms (19.10%) | Memory: 137.9 MB (6.11%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,18 +1,14 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        maximize_alice = 0
-        total = sum(piles)
 
         @cache
-        def solve(i,j, alice_turn):
-            if i>j:
-                return 0
-            if alice_turn:
-                return max(piles[i]+solve(i+1,j,not alice_turn), piles[j]+solve(i,j-1,not alice_turn))
+        def solve(i, j):
+            if i == j:
+                return piles[i]
 
-            else:
-                return min(piles[i]+solve(i+1,j,not alice_turn), piles[j]+solve(i,j-1, not alice_turn))
+            return max(
+                piles[i] - solve(i + 1, j),
+                piles[j] - solve(i, j - 1)
+            )
 
-        
-        maximize_alice = solve(0,len(piles)-1, True)
-        return total-maximize_alice < total/2
+        return solve(0, len(piles) - 1) > 0
