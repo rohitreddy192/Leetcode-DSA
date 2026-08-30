70e4855f8cc2b4979823eb37c8805b2a648d83f9
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-08-30 14:15:47 +0530

    Time: 29 ms (79.88%) | Memory: 21.9 MB (34.41%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, arr, k):
+        mpp = {0: 1}
+        prefix = 0
+        cnt = 0
+
+        for x in arr:
+            prefix += x
+
+            if prefix - k in mpp:
+                cnt += mpp[prefix - k]
+
+            mpp[prefix] = mpp.get(prefix, 0) + 1
+
+        return cnt
