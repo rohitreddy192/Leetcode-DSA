026a0b77d7a5e3d3c69554b9da594a96534ebe98
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-05 00:26:35 +0530

    Time: 171 ms (48.91%) | Memory: 20.6 MB (81.90%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,14 +1,14 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        n = len(piles)
         def isPossible(k):
-            ans = 0
-            for i in range(n):
-                ans += math.ceil(piles[i]/k)
-            return ans<=h
+            cnt = 0
+            for pile in piles:
+                cnt += (math.ceil(pile/k))
+            return cnt<=h
 
-        low, high = 1, max(piles)
-
+        
+        low = 1
+        high = max(piles)
         ans = high
         while low<high:
             mid = (low+high)//2
@@ -17,4 +17,5 @@ class Solution:
                 high = mid
             else:
                 low = mid+1
+        
         return ans
