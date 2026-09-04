fde07ae89e74acd46eef3ba22cc3a53baaecab92
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-05 00:21:11 +0530

    Time: 263 ms (24.26%) | Memory: 26.9 MB (17.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-        rows, cols = len(grid), len(grid[0])
-        visit = set()
-        islands_cnt = 0
-        def bfs(r,c):
-            q = collections.deque()
-            q.append((r,c))
-            visit.add((r,c))
-            while q:
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc in directions:
-                    r,c = row+dr, col+dc
-                    if(r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visit):
-                        q.append((r,c))
-                        visit.add((r,c))
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=="1" and (r,c) not in visit:
-                    bfs(r,c)
-                    islands_cnt += 1
-        return islands_cnt
+        n, m = len(grid), len(grid[0])
+        vis = set()
+        def dfs(vis, i, j):
+            vis.add((i,j))
+            dx, dy = [-1,0,1,0],[0,-1,0,1]
+            for di in range(4):
+                nx, ny = i+dx[di], j+dy[di]
+                if 0<=nx<n and 0<=ny<m and (nx,ny) not in vis and grid[nx][ny]=="1":
+                    dfs(vis,nx,ny)
+        
+        cnt = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]=="1" and (i,j) not in vis:
+                    cnt += 1
+                    dfs(vis,i,j)
+        
+        return cnt
