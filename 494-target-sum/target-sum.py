8b69f71847c3cdcb6456d0225fd2ea9773537cb6
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 00:08:34 +0530

    Time: 167 ms (34.65%) | Memory: 71.5 MB (36.06%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,29 +1,12 @@
 class Solution:
     def findTargetSumWays(self, arr: List[int], target: int) -> int:
 
-        @lru_cache(maxsize=None)
-        def solve(ind , target):
-            if ind == 0: 
-                if arr[0] == 0 and target==0: return 2
-                elif target==0 or target==arr[0]: return 1
-                return 0
+        dp = {}
+        def solve(idx, target):
+            if idx<0: return 1 if target==0 else 0
+            if (idx,target) in dp: return dp[(idx,target)]
+            dp[(idx,target)] =  solve(idx-1, target-arr[idx]) + solve(idx-1, target+arr[idx])
+            return dp[(idx,target)]
+        return solve(len(arr)-1, target)
 
-            pick = 0
             
-            if target-arr[ind]>=0: 
-                pick = solve(ind-1,target-arr[ind])
-
-            not_pick = solve(ind-1,target)
-
-            return pick + not_pick
-
-        n = len(arr)
-        if n==1: 
-            if arr[0]==0 and target==0: return 2
-            return 1 if abs(arr[0])==abs(target) else 0
-        totSum = sum(arr)
-        if totSum - target < 0:
-            return 0
-        if (totSum - target) % 2 == 1:
-            return 0
-        return solve(n-1,(totSum-target)//2)
