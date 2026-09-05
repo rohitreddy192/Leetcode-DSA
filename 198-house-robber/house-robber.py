8b4602dd9d17640f2e9bf6420d8447a379bd05ef
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-05 23:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.75%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,12 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        @cache
-        def solve(i):
-            if i<0: return 0
-            pick = nums[i] + solve(i-2)
-            not_pick = solve(i-1)
-            return max(pick,not_pick)
 
+        dp = {}
+        def solve(idx):
+            if idx < 0: return 0
+            if idx in dp: return dp[idx]
+
+            pick = nums[idx] + solve(idx-2)
+            not_pick = solve(idx-1)
+
+            dp[idx] =  max(pick, not_pick)
+
+            return dp[idx]
         
         return solve(len(nums)-1)
