435f644e00cf6451e52dac78bad9bbb3651df960
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-05 23:35:43 +0530

    Time: 2 ms (23.54%) | Memory: 20.1 MB (7.55%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,10 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        @cache
+        dp = {}
         def solve(i,j):
             if i==0 and j==0: return 1
             if i<0 or j<0: return 0
-            return solve(i-1,j) + solve(i,j-1)
 
+            if (i,j) in dp: return dp[(i,j)]
+            dp[(i,j)] = solve(i-1,j) + solve(i,j-1)
+
+            return dp[(i,j)]
         return solve(m-1,n-1)
