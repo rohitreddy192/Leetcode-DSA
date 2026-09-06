45317ab9e0abf7b0e0b2a4c6ed67c17d4fb54c93
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 07:49:24 +0530

    Time: 847 ms (12.91%) | Memory: 217.3 MB (21.04%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,13 +1,16 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        @cache
+        dp = {}
         def solve(i,j):
             if j<0: return 1
             if i<0 and j>=0: return 0
 
+            if (i,j) in dp: return dp[(i,j)]
             pick = solve(i-1,j-1) if i>=0 and j>=0 and s[i]==t[j] else 0
             not_pick = solve(i-1,j)
-            return pick + not_pick
+
+            dp[(i,j)] = pick + not_pick
+            return dp[(i,j)]
 
         return solve(len(s)-1, len(t)-1)
