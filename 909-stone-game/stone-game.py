8af797964f948ccbc67639c6730b9f0a35f7358e
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 22:52:59 +0530

    Time: 329 ms (19.78%) | Memory: 77 MB (18.65%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,9 +1,14 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
-        # @cache
-        # def solve(l,r):
-        #     if l>=r:
-        #         return 0
 
-        #     return max()
+
+        @cache
+        def solve(l,r):
+            if l>=r:
+                return 0
+            pick_from_left = max(piles[l] + solve(l+1,r-1)-piles[r] ,piles[l] + solve(l+2,r)-piles[l+1])
+            pick_from_right = max(piles[r] + solve(l,r-2)-piles[r-1], piles[r]+ solve(l+1,r-1)-piles[l])
+
+            return max(pick_from_left, pick_from_right)
+        
+        return solve(0,len(piles)-1) > 0
