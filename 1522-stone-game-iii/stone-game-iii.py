7e47b7a1189846bb7436908bb3f27b6a4d5f4f4f
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 23:32:51 +0530

    Time: 1407 ms (9.05%) | Memory: 237.7 MB (5.01%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -1,36 +1,36 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
-        # 1, 2, 3 stones
+
         n = len(stoneValue)
 
         @cache
-        def solve(idx, isAlice):
-            if idx>=n:
+        def solve(idx):
+
+            if idx >= n:
                 return 0
-            
-            if isAlice:
-                ans = float("-inf")
-            else:
-                ans = float("inf")
+
+            ans = float("-inf")
             taken = 0
+
             for i in range(3):
+
                 if idx + i >= n:
                     break
 
-                taken += stoneValue[idx+i]
-                if isAlice:
-                    ans = max(ans, taken + solve(idx+i+1, not isAlice))
-                else:
-                    ans = min(ans, -taken + solve(idx+i+1, not isAlice))
-            
+                taken += stoneValue[idx + i]
+
+                ans = max(
+                    ans,
+                    taken - solve(idx + i + 1)
+                )
+
             return ans
-        
-        ans = solve(0,True)
-        if ans>0:
+
+        ans = solve(0)
+
+        if ans > 0:
             return "Alice"
-        elif ans<0:
+        elif ans < 0:
             return "Bob"
         else:
             return "Tie"
-                    
-                
