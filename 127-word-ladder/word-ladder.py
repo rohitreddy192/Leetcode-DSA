985420810304e52dbe40861fced12685a4fce963
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 18:58:56 +0530

    Time: 315 ms (30.19%) | Memory: 20.5 MB (56.33%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,18 +1,19 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        que = deque()
-        que.append((beginWord,1))
-        lenWord = len(beginWord)
+        dq = deque()
+        dq.append((beginWord,1))
         words = set(wordList)
-        if beginWord in words: words.remove(beginWord)
-        while que:
-            word, steps = que.popleft()
-            for i in 'abcdefghijklmnopqrstuvwxyz':
-                for j in range(lenWord):
-                    tempWord = word[:j]+i+word[j+1:]
-                    if tempWord in words:
-                        words.remove(tempWord)
-                        if tempWord == endWord: 
-                            return steps+1
-                        que.append((tempWord, steps+1))
+        if endWord not in words: return 0
+        while dq: #5000
+            node, steps = dq.popleft()
+            for i in range(97, 97+27): #27
+                ch = chr(i)
+                for j in range(len(node)): #10
+                    newWord = node[:j]+ch+node[j+1:]
+                    if newWord in words:
+                        dq.append((newWord, steps+1))
+                        words.remove(newWord)
+                    if newWord == endWord:
+                        return steps+1
+        
         return 0
