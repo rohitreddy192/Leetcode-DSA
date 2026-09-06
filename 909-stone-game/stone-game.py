f22996b0a2a96135f4c205b35e0a54ab799a65fb
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 22:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (43.85%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,4 +1,9 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         return True
-        
+        # @cache
+        # def solve(l,r):
+        #     if l>=r:
+        #         return 0
+
+        #     return max()
