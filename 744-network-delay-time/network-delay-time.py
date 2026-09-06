ec4a31de1fe6996ec8e9db45d5e715357952a1d7
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 20:29:39 +0530

    Time: 340 ms (53.45%) | Memory: 21.3 MB (55.07%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,27 +1,17 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        hp = []
+        hp= []
         adj = [[] for _ in range(n+1)]
-
-        for u, v, t in times:
-            adj[u].append((v, t))
-
-        heapq.heappush(hp, (0, k))
-
-        dist = defaultdict(lambda: float("inf"))
-        dist[k] = 0
-
+        for u,v,t in times:
+            adj[u].append((v,t))
+        heapq.heappush(hp, (0,k))
+        max_t = defaultdict(lambda: float("inf"))
+        max_t[k] = 0
         while hp:
             time, node = heapq.heappop(hp)
-
-            if time > dist[node]:
-                continue
-
-            for nei, t in adj[node]:
-                new_time = time + t
-
-                if new_time < dist[nei]:
-                    dist[nei] = new_time
-                    heapq.heappush(hp, (new_time, nei))
-
-        return max(dist.values()) if len(dist) == n else -1
+            for nn, t in adj[node]:
+                if max_t[nn] > time + t:
+                    heapq.heappush(hp, (time+t, nn))
+                    max_t[nn] = time+t
+        
+        return max(max_t.values()) if len(max_t.values())==n else -1
