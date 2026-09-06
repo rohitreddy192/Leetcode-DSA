bbce5a87850b01061cdb247d276565f1f276ff8b
Padala Vinay Rohit Reddy <vinayrohitreddypadala12@gmail.com>
2026-09-06 18:35:15 +0530

    Time: 812 ms (5.64%) | Memory: 19.5 MB (34.23%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -3,11 +3,15 @@ class Solution:
         n = len(nums)
         dp = [1]*(n)
         count = [1]*(n)
+
         for i in range(1,n):
+
             for j in range(i):
+
                 if nums[i]>nums[j] and 1+dp[j]>dp[i]:
                     dp[i] = 1+dp[j]
                     count[i] = count[j]
+                    
                 elif nums[i]>nums[j] and 1+dp[j]==dp[i]:
                     count[i] += count[j]
         
